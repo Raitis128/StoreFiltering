e6135eb1d208b4ee6f5a4206c9f4cacd60fdd0ba
Raitis Iziks <raitisiziks@gmail.com>
2024-11-23 06:01:38 +0200

    Add product cards

--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -1,7 +1,37 @@
+import { AiFillStar } from "react-icons/ai";
 import "./products.css";
+import { BsFillBagHeartFill } from "react-icons/bs";
+import data from "../db/data";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <section className="card-container">
+      {data.map((item, index) => (
+        <section className="card" key={index}>
+          <img src={item.img} alt={item.title} className="card-img" />
+          <div className="card-details">
+            <h3 className="card-title">{item.title}</h3>
+            <section className="card-reviews">
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>{item.prevPrice}</del> {item.newPrice}
+              </div>
+
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      ))}
+    </section>
+  );
 };
 
 export default Products;
